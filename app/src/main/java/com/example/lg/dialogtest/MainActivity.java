dce46e62e5199ae165b6f95c9b9d9b0241736cb0
hayoung9000 <wjsgkdud9000@naver.com>
2017-05-25 20:49:04 +0900

    #2 Event Handling

--- app/src/main/java/com/example/lg/dialogtest/MainActivity.java
+++ app/src/main/java/com/example/lg/dialogtest/MainActivity.java
@@ -1,12 +1,13 @@
 package com.example.lg.dialogtest;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
-
+    String[] itemArr= {"김승아","암내","극혐","무좀"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,6 +18,12 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) {
-
+        AlertDialog.Builder dialog=new AlertDialog.Builder(this);
+        dialog.setTitle("첫번째 다이얼로그");
+        dialog.setIcon(R.drawable.like);
+   //   dialog.setMessage("여기는 메시지를 쓰는 곳입니다");
+        dialog.setItems(itemArr,null);
+        dialog.setPositiveButton("Ok",null);
+        dialog.show();
     }
 }
